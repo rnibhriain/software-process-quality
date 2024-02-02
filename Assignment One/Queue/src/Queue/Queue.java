4516b6e7722c0a5d57ff6339a2cd5e4d7cd252f0
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-02-02 19:04:08 +0000

    Finished Queue og imp - next step test

--- Assignment One/Queue/src/Queue/Queue.java
+++ Assignment One/Queue/src/Queue/Queue.java
@@ -1,5 +1,48 @@
 package Queue;
 
 public class Queue {
-
+	
+	private int maxSize;
+	int tail = -1;
+	Object [] list;
+	
+	public void create ( int maxNum ) {
+		maxSize = maxNum;
+		list = new Object[ maxNum ];
+		tail = -1;
+	}
+	
+	public void add ( Object newObj ) {
+		tail++;
+		
+		list[ tail ] = newObj;
+	}
+	
+	public Object remove () {
+		
+		Object current = list[ 0 ];
+		for ( int i = 0; i < tail ; i++ ) {
+			list[ i ] = list[ i + 1 ];
+		}
+		
+		tail--;
+		
+		return current;
+		
+	}
+	
+	public int howMany () {
+		return tail + 1;
+	}
+	
+	public Boolean isEmpty () {
+		
+		if ( howMany() == 0	 ) {
+			
+			return true;
+			
+		}
+		return false;
+	}
+	
 }
---
+++ Assignment One/Queue/src/Queue/QueueTest.java
@@ -0,0 +1,22 @@
+package Queue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QueueTest {
+	
+
+	@Test
+	public void test() {
+
+	}
+	
+	public void testIsEmpty () {
+		
+		
+	}
+	
+	
+
+}
